aec62da90ea267260eb307f80fce68b67de18ad7
Sean Morrissey <morroclearwater@hotmail.com>
2021-11-16 20:20:22 +0800

    Finished NATO project

--- Day27 - Converter/main.py
+++ Day27 - Converter/main.py
@@ -1,21 +1,35 @@
 from tkinter import *
 
+CONVERSION_RATE = 0.14
+
+def convert():
+    if input.get():
+        value['text'] = int(input.get()) * CONVERSION_RATE
+    else:
+        value['text'] = 0
+
+# Setup window
 window = Tk()
-window.title("Super Window")
-window.minsize(width=600, height=400)
+window.title("Converter")
+window.config(padx=20, pady=20)
 
-# Tkinter Label
-my_label = Label(text='My Label', font=('Arial', 24, 'bold'))
-my_label.pack()
+# --- Labels --- #
+cny = Label(text='CNY', font=('Calibri', 12, 'bold'))
+eur = Label(text='EUR', font=('Calibri', 12, 'bold'))
+is_equal = Label(text='is equal to', font=('Calibri', 12, 'bold'))
+value = Label(text='0', font=('Calibri', 12, 'bold'))
+cny.grid(row=0, column=2)
+eur.grid(row=1, column=2)
+is_equal.grid(row=1, column=0)
+value.grid(row=1, column=1)
 
-def button_clicked():
-    my_label['text'] = input.get()
-
+# Input box
 input = Entry(width=10)
-input.pack()
+input.grid(row=0, column=1)
 
-button = Button(text='Confirm', command=button_clicked)
-button.pack()
+# Button
+button = Button(text='Convert', command=convert)
+button.grid(row=2, column=1)
 
 
 
